c3601fc177a8d822278787aa87fced8dde0f65b9
moye <81205313+moye233057@users.noreply.github.com>
2022-08-05 18:01:55 +0800

    1

--- Base/Python/dictknow/dictMethod.md
+++ Base/Python/dictknow/dictMethod.md
@@ -7,3 +7,13 @@ myinfor =  {"name":"qiwsir","site":"qiwsir.github.io","lang":"python"}
 reverseDict = dict(zip(myinfor.values(),myinfor.keys()))
 
 
+json = {"zhangsan": 90, "lisi": 78, "wangermazi": 39}
+# """求字典值的平均"""
+lst = sum(list(json.values())) / len(json)
+# """字典值的排序，利用sorted函数"""
+sor = sorted(json.items(), key=lambda x: x[1], reverse=False)
+turJson = [(json[i], i) for i in json]
+sor1 = sorted(turJson, reverse=False)
+print(lst)
+print(sor)
+print(sor1)
--- Base/Python/mathknow/求解二次方程式.py
+++ Base/Python/mathknow/求解二次方程式.py
@@ -10,3 +10,22 @@ else:
     root2 = (-b - math.sqrt(d)) / (2 * a)
     print("Root 1 = ", root1)
     print("Root 2 = ", root2)
+
+
+def quadratic_equation(a, b, c):
+    if a == 0:
+        return "a不能为0"
+    if any([type(a) != int, type(b) != int, type(c) != int]):
+        return "a，b，c必须为整型"
+    if any([str(a).isdigit() == False, str(b).isdigit() == False, str(c).isdigit() == False]):
+        return "a，b，c不能为非数字"
+    delta = b * b - 4 * a * c
+    if delta < 0:
+        return False
+    elif delta == 0:
+        return -(b / (2 * a))
+    else:
+        sqrt_delta = math.sqrt(delta)
+        x1 = (-b + sqrt_delta) / (2 * a)
+        x2 = (-b - sqrt_delta) / (2 * a)
+        return x1, x2
